e70045f7f8434b4a517504b1f4d5371f22d49f04
sagar-pardhi <sgrpardhi007@gmail.com>
2022-03-05 19:45:26 +0530

    added Login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,68 @@
 import React from "react";
+import { FaSignInAlt } from "react-icons/fa";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [formData, setFormData] = React.useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formData;
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <FaSignInAlt /> Login
+        </h1>
+        <p>Login and start setting goals</p>
+      </section>
+
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              placeholder="Enter your email"
+              value={email}
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              placeholder="Enter your password"
+              value={password}
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="form-grou">
+            <button type="submit" className="btn btn-block">
+              Submit
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
 };
 
 export default Login;
