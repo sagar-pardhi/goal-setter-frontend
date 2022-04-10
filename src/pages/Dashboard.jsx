474ed6b4f247bd0cb725a96f030cffe264835cd0
sagar-pardhi <sgrpardhi007@gmail.com>
2022-04-10 12:18:56 +0530

    updated dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import GoalForm from "../components/GoalForm";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const navigate = useNavigate();
+  const { user } = useSelector((state) => state.auth);
+
+  React.useEffect(() => {
+    if (!user) {
+      navigate("/login");
+    }
+  }, [user, navigate]);
+
+  return (
+    <>
+      <section className="heading">
+        <h1>Welcome. {user && user.name}</h1>
+        <p>Goals Dashboard</p>
+      </section>
+      <GoalForm />
+    </>
+  );
 };
 
 export default Dashboard;
